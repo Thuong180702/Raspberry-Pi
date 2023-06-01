6ac6f9981de237961327c6f797c20960a58632d7
Thuong180702 <vanthuong180702t@gmail.com>
2023-06-01 21:07:21 +0700

    Update

--- Main.py
+++ Main.py
@@ -1,65 +1,15 @@
-import RPi.GPIO as GPIO
+import lcddriver
 import time
-import smbus
 
-# Địa chỉ I2C của LCD
-LCD_ADDRESS = 0x27
-
-# Chân GPIO của cảm biến siêu âm
-TRIG = 23
-ECHO = 24
-
-# Thiết lập chân GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
-
-# Thiết lập giao tiếp I2C với LCD
-bus = smbus.SMBus(1)
+display = lcddriver.lcd()
 
 
-def read_distance():
-    # Gửi xung trigger
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
-
-    # Đọc thời gian trả về của xung echo
-    pulse_start = time.time()
-    while GPIO.input(ECHO) == 0:
-        pulse_start = time.time()
-    pulse_end = time.time()
-    while GPIO.input(ECHO) == 1:
-        pulse_end = time.time()
-
-    # Tính khoảng cách
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150
-    distance = round(distance, 2)
-
-    return distance
+# Hàm chạy chương trình
+def main():
+    display.lcd_display_string("Thuong", 1)
+    display.backlight(1)
 
 
-def display_lcd(message):
-    # Thiết lập LCD vào chế độ 4 dòng
-    bus.write_byte_data(LCD_ADDRESS, 0x00, 0x38)
-    time.sleep(0.05)
-    # Thiết lập LCD hiển thị và con trỏ về vị trí đầu tiên
-    bus.write_byte_data(LCD_ADDRESS, 0x00, 0x0C)
-    time.sleep(0.05)
-    # Gửi dữ liệu đến LCD
-    for char in message:
-        bus.write_byte_data(LCD_ADDRESS, 0x40, ord(char))
-        time.sleep(0.01)
-
-
-try:
-    while True:
-        # Đọc giá trị khoảng cách
-        distance = read_distance()
-        # Hiển thị giá trị lên LCD
-        display_lcd("Distance: {} cm".format(distance))
-        time.sleep(0.5)
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+# Chạy chương trình
+if __name__ == "__main__":
+    main()
